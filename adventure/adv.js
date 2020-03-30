b5e484388599ad700644001589fb910ff2bead6b
Michael Harms <michael.harms6010@gmail.com>
2020-03-30 10:28:09 -0500

    movement working

--- adventure/adv.js
+++ adventure/adv.js
@@ -1,6 +1,7 @@
-let Room = require('./classes/Room');
+const Room = require("./classes/Room")
 let prompt = require("./utils/prompt")
-
+let rooms = require("./utils/rooms");
+let Player = require("./classes/Player.js")
 
 const directions = {
     n: "north",
@@ -8,10 +9,13 @@ const directions = {
     e:"east",
     w: "west"
 }
+let player = new Player( new Room("Outside","You're outside, dude.", 1, 2));
 
-prompt("Indicate your move:", input => {
+
+prompt(`You are in ${player.current_room.name}. ${player.current_room.description} Indicate your move:`, input => {
     if (directions[input]) {
-        console.log(`You move ${directions[input]}.`)
+        player.current_room = rooms.find(item => item.id === player.current_room[`${input}_to`])
+        console.log(`You move ${directions[input]}. You are now in ${player.current_room.name}`)
     }
     process.exit()
 })
--- adventure/utils/rooms.js
+++ adventure/utils/rooms.js
@@ -1,5 +1,8 @@
 const Room = require("../classes/Room")
 
 const rooms = [
-    Room("Outside","You're outside, dude.", 1, 2)
+   new Room("Outside","You're outside, dude.", 1, 2),
+    new Room("Inside","You're inside, dude.", 2, null, 1)
 ]
+
+module.exports = rooms;
